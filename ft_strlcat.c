3b90d770032cfccf7ff32f3b744280399c2e175b
KrebsCoder <lucasklopess@gmail.com>
2021-08-27 19:14:22 -0300

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -5,8 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/20 16:35:42 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/08/20 16:36:31 by lkrebs-l         ###   ########.fr       */
+/*   Created: 2021/08/27 18:46:04 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2021/08/27 19:12:50 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_strlcat(char	*dst, const char	*src, size_t	dstsize)
+{
+	size_t	i;
+	size_t	dest_lenght;
+	size_t	src_lenght;
+
+	dest_lenght = ft_strlen(dst);
+	src_lenght = ft_strlen(src);
+	i = 0;
+	while (dest_lenght + i < dstsize - 1 && src[i] != '\0')
+	{
+		dst[dest_lenght + i] = src[i];
+		i++;
+	}
+	dst[dest_lenght] = 0;
+	if (dest_lenght >= dstsize)
+	{
+		dest_lenght = dstsize;
+	}
+	return (dest_lenght + src_lenght);
+}
