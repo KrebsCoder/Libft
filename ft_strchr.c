8eacf2b676af507bde66208e817d27b467bb7c0a
KrebsCoder <lucasklopess@gmail.com>
2021-08-29 14:56:14 -0300

    libft

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,17 +6,17 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:35:48 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/08/28 19:24:56 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/08/28 20:23:11 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-char *ft_strchr(const char *string, int c)
+char	*ft_strchr(const char *string, int c)
 {
-	while(*string != '\0')
+	while (*string != '\0')
 	{
-		if(*string == c)
+		if (*string == c)
 			return ((char *)string);
 		string++;
 	}
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,28 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:36:41 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/08/20 16:36:41 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/08/29 14:55:42 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+char	*ft_strrchr(const char *string, int c)
+{
+	int	i;
+
+	i = 0;
+	while (*string != '\0')
+	{
+		string++;
+	}
+	while (*string <= string[0])
+	{
+		if (*string == c)
+		{
+			return ((char *) string);
+		}
+		string--;
+	}
+	return (NULL);
+}
