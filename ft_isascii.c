1fbe6678d8f7b3e09cc63f5f40d45bf4c12fd87f
KrebsCoder <lucasklopess@gmail.com>
2021-09-02 07:58:16 -0300

    libft

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,16 +6,15 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:36:10 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/08/30 17:56:56 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/09/01 21:25:15 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isacii(int c)
+int	ft_isascii(int c)
 {
-	while (c != '\0')
-		if (c >= 0 && c <= 127)
-			return (1);
+	if (c >= 0 && c <= 127)
+		return (1);
 	return (0);
 }
