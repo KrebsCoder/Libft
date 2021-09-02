9a87ad429638238b5850093e41b39beb1ca84da0
KrebsCoder <lucasklopess@gmail.com>
2021-09-02 07:43:48 -0300

    libft

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,22 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:36:00 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/08/20 18:09:19 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/09/02 07:43:34 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*cmp1;
+	unsigned char	*cmp2;
+
+	cmp1 = (unsigned char *)s1;
+	cmp2 = (unsigned char *)s2;
+	if (!n)
+		return (0);
+	else if (*cmp1 != *cmp2)
+		return (*cmp1 - *cmp2);
+	return (ft_memcmp(++s1, ++s2, --n));
+}
