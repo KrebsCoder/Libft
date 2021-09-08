74f595ac3394f3c9f320be324d5ff5798c103de6
KrebsCoder <lucasklopess@gmail.com>
2021-09-07 22:12:59 -0300

    libft

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,33 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:35:53 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/08/20 16:36:33 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/09/06 21:48:42 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void    *ft_memmove(void *dest, const void *src, size_t n)
+{
+    size_t    i;
+
+    i = 0;
+    if (dest > src)
+    {
+        while (n > 0)
+        {
+            ((char *)dest)[n - 1] = ((char *)src)[n - 1];
+            n--;
+        }
+    }
+    else
+    {
+        while (n > 0)
+        {
+            ((char *)dest)[i] = ((char *)src)[i];
+            n--;
+            i++;
+        }
+    }
+    return (dest);
+}
