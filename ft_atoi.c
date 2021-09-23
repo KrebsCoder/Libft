598bed34fbfc675eb299746821a41353fed031c7
KrebsCoder <lucasklopess@gmail.com>
2021-09-23 14:55:15 -0300

    libft lkrebs-l

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: krebscoder <krebscoder@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:36:20 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/09/07 21:09:18 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/09/23 14:47:43 by krebscoder       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,22 @@
 int	ft_atoi(const char *str)
 {
 	int	sign;
-	int	i;
 	int	res;
 
-	res = 0;
-	i = 0;
-	sign = 1;
-	while (*str >= 9 && *str <= 13)
+	while ((*str && *str == ' ') || (*str >= 9 && *str <= 13))
 		str++;
+	sign = 1;
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-			sign = -1;
+			sign = -sign;
 		str++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	res = 0;
+	while (*str && *str &&*str >= '0' && *str <= '9')
 	{
-		res = res * 10 + str[i] - '0';
-		i++;
+		res = (res * 10) + (*str - '0');
+		str++;
 	}
 	return (sign * res);
 }
