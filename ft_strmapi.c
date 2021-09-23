e83e510ee61aa3f20486aee2e11e99e20222717a
Krebs <lucasklopess@gmail.com>
2021-09-23 20:43:05 -0300

    libft lkrebs-l

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 14:25:12 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/08/27 14:25:12 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/09/23 20:41:31 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	str = (char *)ft_calloc((ft_strlen(s) + 1), sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
