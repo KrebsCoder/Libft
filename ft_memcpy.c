4dc62fd38eecb3442090848fb947e6a7210a22f9
Krebs <lucasklopess@gmail.com>
2021-09-23 21:38:28 -0300

    libft lkrebs-l

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 16:35:58 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/09/01 20:56:22 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/09/23 21:38:15 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t lenght)
+void	*ft_memcpy(void *dest, const void *src, size_t len)
 {
 	char	*dst;
 	char	*source;
 
 	dst = (char *)dest;
 	source = (char *)src;
-	while (lenght--)
+	while (len--)
 	{
 		*dst++ = *source++;
 	}
