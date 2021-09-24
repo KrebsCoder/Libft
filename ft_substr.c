69d75ee8311807ea154923c686f45aec0fc6ce82
Krebs <lucasklopess@gmail.com>
2021-09-24 17:24:17 -0300

    libft lkrebs-l

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 14:25:35 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/09/14 21:03:00 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/09/24 17:16:47 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,25 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	strlen;
+	size_t	i;
 	char	*substr;
 
-	strlen = ft_strlen(s);
 	if (s == NULL)
 		return (NULL);
+	strlen = ft_strlen(s);
 	if (start > strlen)
 		return (ft_strdup(""));
-	if (strlen - start >= len)
-		substr = (char *)malloc(sizeof(char) * (len + 1));
-	else
-		substr = (char *)malloc((strlen - start + 1) * sizeof(char));
+	if (start + len > strlen)
+		len = strlen - start;
+	substr = malloc((len + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
-	ft_strlcpy(substr, (s + start), (len + 1));
+	i = 0;
+	while (s[start + i] && i < len)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
 	return (substr);
 }
